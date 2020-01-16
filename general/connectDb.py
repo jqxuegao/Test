5946ad05e52169f436083e238c99c25e0098e131
jqxuegao <463628990@qq.com>
2020-01-16 23:16:13 +0800

    链接数据库

---
+++ general/connectDb.py
@@ -0,0 +1,14 @@
+import MySQLdb
+
+# 链接数据库
+db = MySQLdb.connect(host = '139.159.218.173',port = 3306,db = 'gree-dm-application',user = 'root',passwd = 'my-secret-ab',charset = 'utf8')
+# 创建游标
+cursor = db.cursor()
+# 查询语句
+cursor.execute('select * from sys_log')
+# 执行sql
+data = cursor.fetchone()
+# 打印出来
+print(data)
+# 关闭数据库
+db.close()
