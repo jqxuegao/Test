250ea2fa2909c1915fef146e0545986b753ca75c
jqxuegao <463628990@qq.com>
2020-01-16 17:12:50 +0800

    Initial commit

---
+++ general/apiTest.py
@@ -0,0 +1,31 @@
+import requests
+import xlrd
+
+def getRun():
+    url = 'https://tcc.taobao.com/cc/json/mobile_tel_segment.htm?'
+    data = xlrd.open_workbook('C:\\Users\\46362\\Desktop\\test.xlsx')
+    table = data.sheets()[0] # 打开第一张表
+    print (table.name) # 获取sheet名称
+    print (table.ncols) # 获取总列数
+    print (table.nrows) # 获取总行数
+    rows = table.nrows
+
+    for i in range(rows):
+        params = {
+            'tel':table.row_values(i)
+        }
+        response = requests.get(url,params)
+        print(response.text)
+
+getRun()
+
+
+
+
+def postRun():
+    url = ''
+    headers = {
+
+
+    }
+    response = requests.post(url,headers)
---
+++ hello.py
@@ -0,0 +1,12 @@
+class test:
+
+    def myTest1(self):
+        a = 3 + 4
+        return a
+
+    def myTest2(self):
+        b = 4 + 5
+        return b
+
+t = test()
+print(t.myTest1()+t.myTest2())
