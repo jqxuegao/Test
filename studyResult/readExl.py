9c8687a4b1e11580775d03e6db74f30a46ff352c
jqxuegao <463628990@qq.com>
2020-03-04 15:41:56 +0800

    整理读取excel

--- studyResult/readExl.py
+++ studyResult/readExl.py
@@ -1,20 +1,20 @@
-import requests
 import xlrd
 
-def getRun():
-    url = 'https://tcc.taobao.com/cc/json/mobile_tel_segment.htm?'
-    data = xlrd.open_workbook('C:\\Users\\46362\\Desktop\\test.xlsx')
-    table = data.sheets()[0] # 打开第一张sheet1表
-    print (table.name) # 获取sheet名称
-    print (table.ncols) # 获取总列数
-    print (table.nrows) # 获取总行数
-    rows = table.nrows
+def doExl():
 
-    for i in range(rows):
-        params = {
-            'tel':table.row_values(i)
-        }
-        response = requests.get(url,params)
-        print(response.text)
+    # 打开文件
+    data = xlrd.open_workbook(r'C:\Users\46362\Desktop\滴普\test.xls')
+    # 打开第一张sheet1表
+    table = data.sheets()[0]
+    # 获取sheet名称
+    print ("sheet名称：",table.name)
+    # 获取总列数
+    print ("总列数：",table.ncols)
+    # 获取总行数
+    print ("总行数：",table.nrows)
+    # 获取第三行，第二列值
+    value1 = table.cell(2,1).value
+    # 打印
+    print("第三行，第二列值:",value1)
 
-getRun()
+doExl()
