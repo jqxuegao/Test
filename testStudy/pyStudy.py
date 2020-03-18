dd3197d68863b765aad980e6c31804ef35fa6278
xuegao <463628990@qq.com>
2020-03-18 22:43:37 +0800

    初步selenuim

--- testStudy/pyStudy.py
+++ testStudy/pyStudy.py
@@ -1,38 +1,6 @@
-import re
+from selenium import webdriver
 
-def re_test(test):
-    if re.match(r'^\d{3}\-\d{3,8}$',test):
-        print('ok')
-    else:
-        print('no')
-
-re_test('010-12345')
-
-def re_test1(test1):
-    return re.split(r'\s',test1)
-
-print(re_test1('a b  c'))
-
-def re_test2(test1):
-    return re.split(r'\s+',test1)
-
-print(re_test2('a b  c'))
-
-
-def re_test3(test1):
-    return re.split(r'[\s\,]+',test1)
-
-print(re_test3('a,b, c  d'))
-
-def re_test4(test1):
-    return re.split(r'[\s\,\;]+',test1)
-
-print(re_test4('a,b,;;c  d'))
-
-def re_test5(test1):
-    m = re.match(r'^(\d{3})-(\d{3,8})$',test1)
-    print(m.group(0))
-    print(m.group(1))
-    print(m.group(2))
-
-re_test5('010-12345')
+driver = webdriver.Chrome()
+driver.get('http://www.baidu.com')
+driver.find_element_by_id('kw').send_keys('雪糕')
+driver.find_element_by_id('su').click()
