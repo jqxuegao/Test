199796a44a1da2cf5e55fcbbf9ba044daef1c8af
jqxuegao <463628990@qq.com>
2020-04-09 19:56:01 +0800

    做任务数据

--- studyResult/doTestData.py
+++ studyResult/doTestData.py
@@ -1,14 +1,14 @@
 def do_text_data():
 
-    filename = r'C:\Users\Administrator\Desktop\test.txt'
-    mobile_phone = 15000000000
+    filename = r'C:\Users\46362\Desktop\test.txt'
+    mobile_phone = 20000000000
     a = 0
     with open(filename,'w') as file_object:
-        while a < 10000:
-            file_object.write(str(mobile_phone)+' login'+'\n')
-            file_object.write(str(mobile_phone)+' mp_material'+'\n')
-            file_object.write(str(mobile_phone)+' mp_product'+'\n')
-            file_object.write(str(mobile_phone)+' mp_shop'+'\n')
+        while a < 100000:
+            file_object.write(str(mobile_phone)+'\n')
+            # file_object.write(str(mobile_phone)+' mp_material'+'\n')
+            # file_object.write(str(mobile_phone)+' mp_product'+'\n')
+            # file_object.write(str(mobile_phone)+' mp_shop'+'\n')
             mobile_phone += 1
             a += 1
 
